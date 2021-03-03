d0668d6be8eab5becf10b0bc03b229dd2dca0ba2
HaagentiV1 <tristan.claudic.pro@gmail.com>
2021-03-03 10:48:48 +0100

    update likes

--- app/src/main/java/com/example/go4lunch/views/activities/RestaurantDetailActivity.java
+++ app/src/main/java/com/example/go4lunch/views/activities/RestaurantDetailActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,14 @@ import com.example.go4lunch.BuildConfig;
 import com.example.go4lunch.R;
 import com.example.go4lunch.api.UserHelper;
 import com.example.go4lunch.models.PlaceDetail.PlaceDetail;
-import com.example.go4lunch.models.PlaceDetail.PlaceDetailResult;
+import com.example.go4lunch.models.PlaceDetail.Result;
+import com.example.go4lunch.models.User;
 import com.example.go4lunch.presenters.Go4LunchStreams;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -40,11 +45,13 @@ public class RestaurantDetailActivity extends AppCompatActivity {
     @BindView(R.id.detail_restaurant_address)
     public TextView restaurantAddress;
     @BindView(R.id.detail_like_image)
-    public ImageView likeImage;
+    public ImageView likeImageView;
+
 
     private String phone;
     private String website;
-    private List<String> userLikes;
+    private List<String> userLikes = new ArrayList<>();
+    private User currentUser;
 
 
     @OnClick(R.id.detail_restaurant_callButton)
@@ -55,13 +62,26 @@ public class RestaurantDetailActivity extends AppCompatActivity {
 
     @OnClick(R.id.detail_restaurant_websiteButton)
     public void websiteActivity(){
+        if (website != null){
         startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse(website)));
+        }else {
+            Toast.makeText(this,"Website non disponible",Toast.LENGTH_SHORT).show();
+        }
     }
 
     @OnClick(R.id.detail_restaurant_likeButton)
     public void onClickLike(){
-
+        if (userLikes.contains(restaurantId)){
+            UserHelper.deleteLike(getCurrentUserUid(),restaurantId);
+    }else{
+          UserHelper.updateUserLikes(getCurrentUserUid(),restaurantId);
+        }
     }
+    @OnClick(R.id.Reserv_Restaurant_btn)
+    public void onCLickOnReserveRestaurantButton(){
+        UserHelper.updateChosenRestaurant(getCurrentUserUid(),restaurantId);
+    }
+
 
     @Nullable
     protected String getCurrentUserUid(){ return FirebaseAuth.getInstance().getCurrentUser().getUid(); }
@@ -84,12 +104,19 @@ public class RestaurantDetailActivity extends AppCompatActivity {
         }
     }
 
-    public void bindPlaceDetailDataToView(PlaceDetailResult placeDetailResult){
-        Glide.with(this).load("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + placeDetailResult.getPhotos().get(0).getPhotoReference() + "&key=" + BuildConfig.API_KEY).into(restaurantPicture);
+    private void setRestaurantPicture(Result placeDetailResult){
+        if (placeDetailResult.getPhotos() != null && !placeDetailResult.getPhotos().isEmpty()){
+            Glide.with(this).load("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + placeDetailResult.getPhotos().get(0).getPhotoReference() + "&key=" + BuildConfig.API_KEY).into(restaurantPicture);
+        }else {restaurantPicture.setImageResource(R.drawable.image_not_available);}
+
+    }
+
+    public void bindPlaceDetailDataToView(Result placeDetailResult){
         restaurantName.setText(placeDetailResult.getName());
         restaurantAddress.setText(placeDetailResult.getFormattedAddress());
         phone = placeDetailResult.getFormattedPhoneNumber();
-        website = placeDetailResult.getWebsite();
+        website = placeDetailResult.getUrl();
+        likeImageView.setBackgroundResource(R.drawable.ic_baseline_star_rate_24);
         Log.e("website", "websiteurl" + website);
         Log.e("Tag","restoid" + restaurantId);
 
@@ -99,17 +126,19 @@ public class RestaurantDetailActivity extends AppCompatActivity {
         Log.e("httpid","id" + restaurantId);
         disposable = Go4LunchStreams.streamFetchDetails(restaurantId).subscribeWith(new DisposableObserver<PlaceDetail>() {
             @Override
-            public void onNext(@NonNull PlaceDetail placeDetail) {
+            public void onNext(PlaceDetail placeDetail) {
                 Log.e("detailHttp","detailObject" + placeDetail.getStatus());
-                PlaceDetailResult placeDetailResult = placeDetail.getResult();
+                Log.e("test","test" + placeDetail.getResult().getPlaceId());
+                Result placeDetailResult = placeDetail.getResult();
                 bindPlaceDetailDataToView(placeDetailResult);
+                setRestaurantPicture(placeDetailResult);
+                Log.e("Tag","getType" + placeDetail.getResult().getTypes());
 
             }
 
             @Override
             public void onError(@NonNull Throwable e) {
                 Log.e("error","error" + e);
-
             }
 
             @Override
@@ -120,7 +149,17 @@ public class RestaurantDetailActivity extends AppCompatActivity {
     }
 
     public void getUserFromFirebaseDatabase(){
-        UserHelper.getUser(getCurrentUserUid());
+        UserHelper.getUser(getCurrentUserUid()).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                currentUser = documentSnapshot.toObject(User.class);
+                userLikes = currentUser.getLikes();
+            }
+        });
+    }
+
+    public void getUserLikes(){
+
     }
 
     public boolean checkLikes(@Nullable List<String> userLikes){
@@ -130,12 +169,6 @@ public class RestaurantDetailActivity extends AppCompatActivity {
             }
         }return false;
     }
-    public void updateLikeUi(){
-        if (checkLikes(userLikes)){
-            likeImage.setColorFilter(getApplicationContext().getResources().getColor(R.color.yellow_gold));
-        }else {
-            likeImage.setColorFilter(getApplicationContext().getResources().getColor(R.color.white));
-        }
-    }
+
 
 }
