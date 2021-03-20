241ca1cd435837e1697d14cebf6a496633597800
HaagentiV1 <tristan.claudic.pro@gmail.com>
2021-03-20 20:02:54 +0100

    Update userModel to add Restaurant name

--- app/src/main/java/com/example/go4lunch/models/User.java
+++ app/src/main/java/com/example/go4lunch/models/User.java
@@ -3,6 +3,7 @@ package com.example.go4lunch.models;
 import androidx.annotation.Nullable;
 
 import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.ServerTimestamp;
 
 import java.util.List;
 
@@ -15,18 +16,20 @@ public class User {
     private Timestamp userCreationTimestamp;
     private String chosenRestaurant;
     private Timestamp chosenRestaurantTimestamp;
+    private String chosenRestaurantName;
     public User(){ }
 
 
 
 
-    public User(String uid, String username, String urlPicture, List<String> likes, Timestamp userCreationTimestamp, String chosenRestaurant, Timestamp chosenRestaurantTimestamp){
+    public User(String uid, String username, @Nullable String urlPicture, List<String> likes, Timestamp userCreationTimestamp, String chosenRestaurant, String chosenRestaurantName, Timestamp chosenRestaurantTimestamp){
         this.uid = uid;
         this.username = username;
         this.urlPicture = urlPicture;
         this.likes = likes;
         this.userCreationTimestamp = userCreationTimestamp;
         this.chosenRestaurant = chosenRestaurant;
+        this.chosenRestaurantName = chosenRestaurantName;
         this.userCreationTimestamp = chosenRestaurantTimestamp;
     }
 
@@ -36,8 +39,10 @@ public class User {
     public String getUsername(){ return username; }
     public String getUrlPicture() {return urlPicture; }
     public List<String> getLikes() { return likes; }
+    @ServerTimestamp
     public Timestamp getUserCreationTimestamp() { return userCreationTimestamp; }
     public String getChosenRestaurant() { return chosenRestaurant; }
+    public String getChosenRestaurantName() {return chosenRestaurantName;}
     public Timestamp getChosenRestaurantTimestamp() { return chosenRestaurantTimestamp; }
 
 
@@ -50,5 +55,6 @@ public class User {
     public void setUserCreationTimestamp(Timestamp userCreationTimestamp) { this.userCreationTimestamp = userCreationTimestamp; }
     public void setChosenRestaurant(String chosenRestaurant) { this.chosenRestaurant = chosenRestaurant; }
     public void setChosenRestaurantTimestamp(Timestamp chosenRestaurantTimestamp) { this.chosenRestaurantTimestamp = chosenRestaurantTimestamp; }
+    public void setChosenRestaurantName (String chosenRestaurantName){this.chosenRestaurantName = chosenRestaurantName;}
 
 }
