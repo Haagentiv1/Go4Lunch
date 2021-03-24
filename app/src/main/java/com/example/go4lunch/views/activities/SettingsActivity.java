367db6f75992a6773db96c08eaf195d3584748fe
HaagentiV1 <tristan.claudic.pro@gmail.com>
2021-03-24 18:40:08 +0100

    suppress unused method

--- app/src/main/java/com/example/go4lunch/views/activities/SettingsActivity.java
+++ app/src/main/java/com/example/go4lunch/views/activities/SettingsActivity.java
@@ -3,12 +3,10 @@ package com.example.go4lunch.views.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.go4lunch.BaseActivity;
 import com.example.go4lunch.R;
-import com.example.go4lunch.api.UserHelper;
 
 public class SettingsActivity extends BaseActivity {
 
@@ -35,18 +33,9 @@ public class SettingsActivity extends BaseActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
+
+
     }
 
-    private void deleteUserFromFirebase(){
-    if (this.getCurrentUser() != null){
-        UserHelper.deleteUser(this.getCurrentUser().getUid()).addOnFailureListener(this.onFailureListener());
-    }
-    }
-
-    private void updateUserNotification(){
-        if (this.getCurrentUser() != null){
-            UserHelper.updateNotification(this.getCurrentUser().getUid(),mNotification);
-        }
-    }
 }
 
